b8800bc6fb4a381b95f57d93168056ccdf516fb7
hefangpu <cracktit@yahoo.com>
2018-04-27 00:33:27 +0800

    Rollback & Open Doors

---
+++ Ephemeral/Assets/OpenKeyDoors.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenKeyDoors : MonoBehaviour {
+	public ItemBox itembox;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		if (Physics.Raycast(MouseHighlight.ray, out hit, 1.0f))
+		{
+			TryOpen(this.gameObject);
+		}
+	}
+
+	void TryOpen(GameObject obj){
+		if (Input.GetKeyDown(KeyCode.F))
+		{
+			if (itembox.isExist (1)) {
+				Debug.Log ("u shall pass!");
+			} else {
+				Debug.Log ("u shall not pass!");
+			}
+		}
+	}
+}
