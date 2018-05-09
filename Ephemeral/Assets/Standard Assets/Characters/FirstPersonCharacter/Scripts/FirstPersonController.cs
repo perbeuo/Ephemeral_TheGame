0f56f9d49c35724b627b99bcf91a3147b6eb7232
changhao <1037855969@qq.com>
2018-05-09 13:35:50 +0800

    elevatorchange

--- Ephemeral/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Ephemeral/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -41,9 +41,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         private float m_NextStep;
         private bool m_Jumping;
         private AudioSource m_AudioSource;
-        //public GameObject door;
-        public Animator dooranimator;
-        // Use this for initialization
+        
         private void Start()
         {
             m_CharacterController = GetComponent<CharacterController>();
@@ -56,7 +54,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
             m_Jumping = false;
             m_AudioSource = GetComponent<AudioSource>();
 			m_MouseLook.Init(transform , m_Camera.transform);
-            //dooranimator = door.GetComponent<Animator>();
         }
 
 
@@ -83,7 +80,11 @@ namespace UnityStandardAssets.Characters.FirstPerson
             }
 
             m_PreviouslyGrounded = m_CharacterController.isGrounded;
+            if (Input.GetKeyDown(KeyCode.C))
+            {
 
+            }
+            
         }
 
 
@@ -259,11 +260,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
             body.AddForceAtPosition(m_CharacterController.velocity*0.1f, hit.point, ForceMode.Impulse);
         }
 
-        public void Dooropen()
-        {
-       
-            dooranimator.SetBool("status",true);
-        }
+
 
     }
 }
