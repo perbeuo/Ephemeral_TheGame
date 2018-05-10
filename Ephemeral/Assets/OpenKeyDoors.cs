c880b555a9f46414bdd5e6f0ee439ddec99b6a28
hefangpu <cracktit@yahoo.com>
2018-05-10 20:11:20 +0800

    Finish Initial Scene's Dialogue

--- Ephemeral/Assets/OpenKeyDoors.cs
+++ Ephemeral/Assets/OpenKeyDoors.cs
@@ -14,11 +14,11 @@ public class OpenKeyDoors : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		RaycastHit hit;
-		if (Physics.Raycast(MouseHighlight.ray, out hit, 1.6f))
-		{
-			TryOpen(this.gameObject);
-		}
+//		RaycastHit hit;
+//		if (Physics.Raycast(MouseHighlight.ray, out hit, 1.6f))
+//		{
+//			TryOpen(this.gameObject);
+//		}
 	}
 
 	void TryOpen(GameObject obj){
@@ -29,9 +29,13 @@ public class OpenKeyDoors : MonoBehaviour {
 					Application.LoadLevel("hospital");
 				}
 				//Debug.Log ("u shall pass!");
-			} else {
-				Debug.Log ("u shall not pass!");
 			}
+		}
+	}
+
+	public static void LoadScene(int num){
+		if (num == 1) {
+			Application.LoadLevel("hospital");
 		}
 	}
 }
